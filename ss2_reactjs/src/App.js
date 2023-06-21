5bb2ade131af50548335398c46f93198384587fd
phandoantri <pdtri1996@gmail.com>
2023-06-21 19:34:54 +0700

    module5

---
+++ ss2_reactjs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {ListBlog} from "./component/Blog";
+import React from "react";
+
+function App() {
+    return (
+        <>
+            <ListBlog/>
+        </>
+    );
+}
+
+export default App;
